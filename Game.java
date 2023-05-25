55e9b00efc234ae7a1d94cfcafc4a77ee1c4bae1
土井 朋哉 (Tomoya Doi) <doi.t@wingarc.com>
2023-05-25 13:21:17 +0900

    たたきだいつくりました

--- Game.java
+++ Game.java
@@ -1,20 +1,25 @@
 import java.io.File;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.Set;
 
 public class Game{
     private String dirname;
     private List<String> fileNames;
     private Set<String> challengedFileNamesSet;
+    private Random rand;
 
     public Game(String dirname) {
         this.dirname = dirname;
 
         fileNames = getAllQuestionFileNames();
         challengedFileNamesSet = new HashSet<String>();
+
+        rand = new Random(LocalDateTime.now().getNano());
     }
 
     // {dirName}ディレクトリに含まれるすべてのファイル名を取得し、そのリストを返す
@@ -32,7 +37,7 @@ public class Game{
         if(challengedFileNamesSet.size() == fileNames.size()){
             throw new RuntimeException("既にすべての問題を出題しています");
         }
-        int fileIndex = new Random().nextInt(fileNames.size());
+        int fileIndex = rand.nextInt(fileNames.size());
         String fileName = fileNames.get(fileIndex);
         if(challengedFileNamesSet.contains(fileName)){
             return getRandomFileName();
@@ -42,6 +47,22 @@ public class Game{
     }
 
     public void run() {
-        // TODO
+        System.out.println("クイズです");
+        
+        int solved = 0, challenged = 0;
+        // Scanner scanner = new Scanner(System.in);
+        // while(true){
+
+        //     long startTime = System.currentTimeMillis();
+        //     while (System.currentTimeMillis() - startTime < 10000) {
+        //         if (scanner.hasNextLine()) {
+        //             String line = scanner.nextLine();
+        //             System.out.println("You entered: " + line);
+        //             break;
+        //         }
+        //     }
+        // }
+        
+        // scanner.close();
     }
 }
